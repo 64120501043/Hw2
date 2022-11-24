f10cb4c17a75dab27f60e845e58da7614476cac5
Moji <111563794+64120501043@users.noreply.github.com>
2022-11-24 23:25:10 +0700

    Add files via upload

---
+++ HW2/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<char> Thai = new Queue<char>();
+        while(true){
+            Console.WriteLine("----------------");
+            Console.WriteLine("L / M / S ");
+            Console.WriteLine("Your Size : ");
+            Console.WriteLine("----------------");
+            char Fruit = char.Parse(Console.ReadLine());
+            if (Fruit != 'L' && Fruit != 'M' && Fruit != 'S'){
+                break;
+            }
+            else{
+                Thai.Push(Fruit);
+            }
+        } 
+        while(Thai.GetLength() != 0){
+            char FLove = Thai.Pop();
+
+            if(FLove == 'L'){
+                Console.Write("1");
+                Thai.Push('M');
+                Thai.Push('M');
+            }
+            else if(FLove == 'M'){
+                Console.Write("2");
+                Thai.Push('S');
+                Thai.Push('S');
+                Thai.Push('S');
+            }
+            else if(FLove == 'S'){
+                Console.Write("3");
+            }
+        }
+    }
+}
